c0437c06904f4aff45a32e181162f0b377b1355d
SaudAlzahri <113842486+SaudAlzahri@users.noreply.github.com>
2023-07-04 23:06:19 +0300

    Update bcode_s.py

--- bcode_s.py
+++ bcode_s.py
@@ -1,1 +1,13 @@
+import streamlit as st
+
+
+ ## WEB CONFIG
+st.set_page_config(layout="wide", page_title="scanner",menu_items={'About': 'google.com'})
+st.header("scanner")
+
+x = '<p>fsd</p>'
+
+st.markdown(str(x), unsafe_allow_html=True)
+
+
 
