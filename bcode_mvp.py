24ea9c18dc9d15322493b71c975e310b06fa3310
SaudAlzahri <113842486+SaudAlzahri@users.noreply.github.com>
2023-07-04 23:19:36 +0300

    Update bcode_mvp.py

--- bcode_mvp.py
+++ bcode_mvp.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="scanner",menu_items={'About': 'google.com'})
 st.header("scanner")
 
-x = '<p>fsd</p>'
+x = '<p>fs</p>'
 
 st.markdown(str(x), unsafe_allow_html=True)
 x = '<p>fsdd</p>'
